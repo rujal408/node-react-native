f9142a8f710b1cc11e39df1bae0b772da36cd0eb
rujal408 <rujal408@gmail.com>
2024-08-25 14:08:01 +0545

    add font configuration

--- Frontend/app/index.tsx
+++ Frontend/app/index.tsx
@@ -1,11 +1,42 @@
 import { Text, View } from "react-native";
-import React from "react";
-import { Link } from "expo-router";
+import React, { useEffect } from "react";
+import { Link, SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 const HomePage = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-red">
-      <Text className="text-3xl">HomePage</Text>
+      <Text className="text-3xl font-pblack">HomePage</Text>
       <Link href="/profile" className="text-blue-500">
         Go to profile
       </Link>
