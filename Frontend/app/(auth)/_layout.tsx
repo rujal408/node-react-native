33ff2a22a831c86a7ce2c1643f98986effc78e2a
rujal408 <rujal408@gmail.com>
2024-08-29 11:49:48 +0545

    working on auth

--- Frontend/app/(auth)/_layout.tsx
+++ Frontend/app/(auth)/_layout.tsx
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 const AuthRootLayout = () => {
   return (
-    <View>
-      <Text>AuthRootLayout</Text>
-    </View>
-  )
-}
+    <>
+      <Stack>
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
+  );
+};
 
-export default AuthRootLayout
+export default AuthRootLayout;
--- Frontend/app/(auth)/sign-in.tsx
+++ Frontend/app/(auth)/sign-in.tsx
@@ -1,11 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full h-full justify-center min-h-[85vh] px-4 my-6">
+          <Image source={images.logo} resizeMode="contain" />
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
